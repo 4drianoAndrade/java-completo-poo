05e1624d9ac72fc33286bf1e258c029b3aadbf74
Adriano Andrade <adrandrade1995@gmail.com>
2021-08-16 22:58:21 -0300

    Atualiza ate setAutoCommit, commit e rollback - secao 21

--- conteudo-das-aulas/secao-03_introducao-a-linguagem-java/001-saida-de-dados-na-tela/src/Main.java
+++ conteudo-das-aulas/secao-03_introducao-a-linguagem-java/001-saida-de-dados-na-tela/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// Ao escrever syso ou sysout, mais control + space, a IDE printa na tela o System.out.println();
-		System.out.println("Ol· mundo!");
+		System.out.println("Ol√° mundo!");
 		
-		// F11 compila e executa o programa de forma r·pida.
+		// 'Ctrl + F11' compila e executa o programa de forma r√°pida.
 	}
 }

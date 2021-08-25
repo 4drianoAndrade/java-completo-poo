f9e62e6e77bd1fc878e204df88feb7d9066341a6
Adriano Andrade <adrandrade1995@gmail.com>
2021-08-24 22:35:01 -0300

    Finaliza a secao 21 - Acesso a banco de dados com JDBC

---
+++ conteudo-das-aulas/secao-21_acesso-a-banco-de-dados-com-jdbc/161-tentativa-de-melhorar-o-projeto/src/model/dao/Dao.java
@@ -0,0 +1,20 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface Dao<T> {
+
+	void insert(T obj);
+
+	void update(T obj);
+
+	void deleteById(Integer id);
+
+	T findById(Integer id);
+
+	List<T> findAll();
+
+	List<T> findByDepartment(Department obj);
+}
